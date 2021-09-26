7b942d90ffac5a64893be886770418dbad55b8df
KarenAlexa99 <kokaren.18@hotmail.com>
2021-09-25 20:22:44 -0500

    switch character system
    -cambiar personajes con 1,2,3.
    -powerups del personaje 2 y 3

---
+++ Assets/Scripts/SwitchCharacterSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacterSystem : MonoBehaviour
+{
+    public GameObject character1, character2, character3;
+    
+
+    void Start()
+    {
+        character1.SetActive(true);
+        character2.SetActive(false);
+        character3.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            character1.SetActive(true);
+            character2.SetActive(false);
+            character3.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            character1.SetActive(false);
+            character2.SetActive(true);
+            character3.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            character1.SetActive(false);
+            character2.SetActive(false);
+            character3.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ZorroPowe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZorroPowe : MonoBehaviour
+{
+
+    public float multiplier = 0.8f;
+    public float duration = 4f;
+    public GameObject player;
+    public float powerRate;
+    private float nextpower;
+
+    void Start()
+    {
+
+
+    }
+
+    public void Update()
+    {
+         if (Input.GetMouseButtonDown(0) && Time.time > nextpower) {
+            nextpower = Time.time + powerRate;
+            player.transform.localScale *= multiplier;
+
+        }
+    }
+
+    
+
+
+}
