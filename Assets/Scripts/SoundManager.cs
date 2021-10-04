1b4b2cfbd1001738d00b9a398fc24c8544916630
KarenAlexa99 <kokaren.18@hotmail.com>
2021-10-03 22:59:09 -0500

    rezar

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip powerShieldSound;
+    static AudioSource audioSrc;
+    void Start()
+    {
+        powerShieldSound = Resources.Load<AudioClip>("shield");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public static void Playsound (string clip)
+    {
+        switch (clip)
+        {
+            case "shield":
+                audioSrc.PlayOneShot(powerShieldSound);
+                break;
+        }
+    }
+}
